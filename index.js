a957e1a9ff6cfd68eed080489eb63503c4a2819a
Win Feigle <win.feigle@gmail.com>
2022-05-22 12:38:57 -0600

    commit tree 75289786e2c54957c9fa1421b787894d511edff4

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(array){
+    const championships = array.find(season =>
+        season.result === 'W'
+    )
+    return championships ? championships.year : undefined;
+}
+
